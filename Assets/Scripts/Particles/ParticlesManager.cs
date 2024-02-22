dc28a1dd40f06682a4959ccec54937bf78a0b616
sofia.savilampi <sofia.savilampi@gmail.com>
2024-02-22 19:07:01 +0000

    Updated compression a bit.

--- Assets/Scripts/Particles/ParticlesManager.cs
+++ Assets/Scripts/Particles/ParticlesManager.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.InteropServices;
-using System.Runtime.Serialization.Formatters.Binary;
 using BitStreams;
 using RocketNet;
 using UnityEngine;
@@ -11,24 +9,30 @@ using Random = UnityEngine.Random;
 
 public class ParticlesManager : MonoBehaviour
 {
+    // Class to store position and bit data of a trunk
     public class TrunkPosition
     {
-        public Vector3b Position;
-        public Vector3b Bits;
-        public TrunkPosition( Vector3b position, Vector3b bits)
+        public Vector3b Position; // Position encoded as Vector3b
+        public Vector3b Bits; // Bit data for position encoding
+        public TrunkPosition(Vector3b position, Vector3b bits)
         {
             Position = position;
             Bits = bits;
         }
     }
-    public class BakedParticles {
+
+    // Class to store baked particles data
+    public class BakedParticles
+    {
         public int TotalFrames;
         public int ParticlesPerFrame;
         public float TotalTime;
         public TrunkPosition[] StartingPositions = new TrunkPosition[1200];
-        public byte[] Bytestream = new byte[1024 * 1024 * 2];
+        public byte[] Bytestream = new byte[1024 * 1024 * 8];
         public int MaxBitstream;
     }
+
+    // Tracks for particle effects
     private Track _orbitX;
     private Track _orbitY;
     private Track _orbitZ;
@@ -42,34 +46,35 @@ public class ParticlesManager : MonoBehaviour
     private Track _windX;
     private Track _windY;
     private Track _windZ;
+
+    // Baked particles data and other variables
     private BakedParticles _bakedParticles;
     private Vector3[] _startingPositions;
     private Dictionary<int, List<Vector3>> _deltaChange;
     [SerializeField] private DeviceController _deviceController;
     [SerializeField] private ParticleSystem particleSystem;
     public List<byte> DeltaMovement;
+
+    // Struct to represent a particle
     public struct Particle
     {
         public Vector3 position;
         public Vector3 velocity;
     }
 
+    // Compute shader for particle simulation
     public ComputeShader computeShader;
-
     ComputeBuffer particles;
-
     const int WARP_SIZE = 1024;
-
     int size = 1024000;
     int stride;
-
     int warpCount;
-
     int kernelIndex;
-
     Particle[] initBuffer;
     private float bakeTime = 0f;
     string filePath;
+
+    // Method to save baked particles data to a file
     void SaveObject(BakedParticles obj)
     {
         using (FileStream file = File.Create(filePath))
@@ -79,7 +84,7 @@ public class ParticlesManager : MonoBehaviour
                 writer.Write(obj.TotalFrames);
                 writer.Write(obj.TotalTime);
                 writer.Write(obj.ParticlesPerFrame);
-                for(int i = 0; i < 1200; i++) 
+                for (int i = 0; i < 1200; i++)
                 {
                     var startPosition = _bakedParticles.StartingPositions[i];
                     writer.Write(startPosition.Bits.X);
@@ -89,7 +94,7 @@ public class ParticlesManager : MonoBehaviour
                     writer.Write(startPosition.Position.Y);
                     writer.Write(startPosition.Position.Z);
                 }
-                for(int i = 0; i < obj.MaxBitstream; i++)
+                for (int i = 0; i < obj.MaxBitstream; i++)
                 {
                     var strbyte = obj.Bytestream[i];
                     writer.Write(strbyte);
@@ -100,34 +105,81 @@ public class ParticlesManager : MonoBehaviour
         }
     }
 
-    // Use this for initialization
+    // Method to count the number of bits required to represent a value
+    public static int CountBits(int value)
+    {
+        // Special case for zero
+        if (value == 0)
+            return 1;
 
-    void Start ()
+        // Calculate the number of bits required
+        return (int)Math.Floor(Math.Log(Math.Abs(value), 2)) + 1;
+    }
+
+    // Method to compress particle data
+    void ParticleCompression()
+    {
+        var bitstream = new BitStreams.BitStream(_bakedParticles.Bytestream);
+        int[] scale = new int[particleSystem.main.maxParticles * 3 + 3];
+        foreach (var deltaDict in _deltaChange)
+        {
+            var deltaArray = deltaDict.Value;
+            for (int i = 0; i < deltaArray.Count; i++)
+            {
+                byte x = (byte)((byte)(Mathf.FloorToInt(Mathf.Abs(deltaArray[i].x * 16f)) << 1) + (byte)((Mathf.Sign(deltaArray[i].x) > 0) ? 1 : 0));
+                byte y = (byte)((byte)(Mathf.FloorToInt(Mathf.Abs(deltaArray[i].y * 16f)) << 1) + (byte)((Mathf.Sign(deltaArray[i].y) > 0) ? 1 : 0));
+                byte z = (byte)((byte)(Mathf.FloorToInt(Mathf.Abs(deltaArray[i].z * 16f)) << 1) + (byte)((Mathf.Sign(deltaArray[i].z) > 0) ? 1 : 0));
+                DeltaMovement.Add(x);
+                DeltaMovement.Add(y);
+                DeltaMovement.Add(z);
+                scale[i * 3 + 0] = Math.Max(CountBits(x), scale[i * 3 + 0]);
+                scale[i * 3 + 1] = Math.Max(CountBits(y), scale[i * 3 + 1]);
+                scale[i * 3 + 2] = Math.Max(CountBits(z), scale[i * 3 + 2]);
+                Debug.Log("Scale:" + scale[i * 3 + 0]);
+            }
+        }
+        for (int i = 0; i < _startingPositions.Length; i++)
+        {
+            byte x = (byte)((byte)(Mathf.FloorToInt(Mathf.Abs(_startingPositions[i].x * 16f)) << 1) + (byte)((Mathf.Sign(_startingPositions[i].x) > 0) ? 1 : 0));
+            byte y = (byte)((byte)(Mathf.FloorToInt(Mathf.Abs(_startingPositions[i].y * 16f)) << 1) + (byte)((Mathf.Sign(_startingPositions[i].y) > 0) ? 1 : 0));
+            byte z = (byte)((byte)(Mathf.FloorToInt(Mathf.Abs(_startingPositions[i].z * 16f)) << 1) + (byte)((Mathf.Sign(_startingPositions[i].z) > 0) ? 1 : 0));
+            _bakedParticles.StartingPositions[i] = (new TrunkPosition(new Vector3b(x, y, z), new Vector3b((byte)scale[i * 3 + 0], (byte)scale[i * 3 + 1], (byte)scale[i * 3 + 2])));
+        }
+        for (int i = 0; i < DeltaMovement.Count; i++)
+        {
+            var howManyBits = scale[i % (particleSystem.main.maxParticles * 3)];
+            bitstream.WriteByte(DeltaMovement[i], howManyBits);
+            if (i % (particleSystem.main.maxParticles * 3) == 0)
+            {
+                Debug.Log("BitOffset:" + bitstream.BitOffsetPosition / 8);
+            }
+        }
+        Debug.Log(bitstream.BitOffsetPosition);
+        _bakedParticles.MaxBitstream = (int)bitstream.BitOffsetPosition;
+        SaveObject(_bakedParticles);
+    }
+
+    // Initialization
+    void Start()
     {
         if (_deviceController.InTrackRecordMode)
         {
             Application.targetFrameRate = 25;
         }
         warpCount = Mathf.CeilToInt((float)size / WARP_SIZE);
-
         stride = Marshal.SizeOf(typeof(Particle));
         particles = new ComputeBuffer(size, stride);
-
         initBuffer = new Particle[particleSystem.main.maxParticles];
         _startingPositions = new Vector3[particleSystem.main.maxParticles];
         for (int i = 0; i < particleSystem.main.maxParticles; i++)
         {
             initBuffer[i] = new Particle();
-            _startingPositions[i] = initBuffer[i].position = new Vector3(Random.Range(-10.0f,10.0f), Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f));
+            _startingPositions[i] = initBuffer[i].position = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f));
             initBuffer[i].velocity = Vector3.zero;
         }
-
         particles.SetData(initBuffer);
-
         kernelIndex = computeShader.FindKernel("Update");
-        
         computeShader.SetBuffer(kernelIndex, "Particles", particles);
-
         _orbitX = _deviceController.Device.GetTrack("Orbit X");
         _orbitY = _deviceController.Device.GetTrack("Orbit Y");
         _orbitZ = _deviceController.Device.GetTrack("Orbit Z");
@@ -147,7 +199,6 @@ public class ParticlesManager : MonoBehaviour
         _windX = _deviceController.Device.GetTrack("Wind X");
         _windY = _deviceController.Device.GetTrack("Wind Y");
         _windZ = _deviceController.Device.GetTrack("Wind Z");
-
         _bakedParticles = new BakedParticles();
         _bakedParticles.TotalFrames = Mathf.CeilToInt(_deviceController.GetAudioSource().clip.length * 25);
         _bakedParticles.TotalTime = _deviceController.GetAudioSource().clip.length;
@@ -157,59 +208,10 @@ public class ParticlesManager : MonoBehaviour
         DeltaMovement = new List<byte>();
         _deltaChange = new Dictionary<int, List<Vector3>>();
     }
-    public static int CountBits(int value)
-    {
-        // Special case for zero
-        if (value == 0)
-            return 1;
 
-        // Calculate the number of bits required
-        return (int)Math.Floor(Math.Log(Math.Abs(value), 2)) + 1;
-    }
-    void ParticleCompression()
+    // Update is called once per frame
+    void Update()
     {
-        var bitstream = new BitStreams.BitStream(_bakedParticles.Bytestream);
-        int[] scale = new int[particleSystem.main.maxParticles * 3 + 3];
-        foreach (var deltaDict in _deltaChange)
-        {
-            var deltaArray = deltaDict.Value;
-            for (int i = 0; i < deltaArray.Count; i++)
-            {
-                byte x = (byte)((byte)(Mathf.FloorToInt(Mathf.Abs(deltaArray[i].x * 32f)) << 1) + (byte)((Mathf.Sign(deltaArray[i].x) > 0) ? 1 : 0));
-                byte y = (byte)((byte)(Mathf.FloorToInt(Mathf.Abs(deltaArray[i].y * 32f)) << 1) + (byte)((Mathf.Sign(deltaArray[i].y) > 0) ? 1 : 0));
-                byte z = (byte)((byte)(Mathf.FloorToInt(Mathf.Abs(deltaArray[i].z * 32f)) << 1) + (byte)((Mathf.Sign(deltaArray[i].z) > 0) ? 1 : 0));
-                DeltaMovement.Add(x);
-                DeltaMovement.Add(y);
-                DeltaMovement.Add(z);
-                scale[i * 3 + 0] = Math.Max(CountBits(x), scale[i * 3 + 0]);
-                scale[i * 3 + 1] = Math.Max(CountBits(y), scale[i * 3 + 1]);
-                scale[i * 3 + 2] = Math.Max(CountBits(z), scale[i * 3 + 2]);
-                Debug.Log("Scale:" + scale[i * 3 + 0]);
-            }
-        }
-        for (int i = 0; i < _startingPositions.Length; i++)
-        {
-            byte x = (byte)((byte)(Mathf.FloorToInt(Mathf.Abs(_startingPositions[i].x * 8f)) << 1) + (byte)((Mathf.Sign(_startingPositions[i].x) > 0) ? 1 : 0));
-            byte y = (byte)((byte)(Mathf.FloorToInt(Mathf.Abs(_startingPositions[i].y * 8f)) << 1) + (byte)((Mathf.Sign(_startingPositions[i].y) > 0) ? 1 : 0));
-            byte z = (byte)((byte)(Mathf.FloorToInt(Mathf.Abs(_startingPositions[i].z * 8f)) << 1) + (byte)((Mathf.Sign(_startingPositions[i].z) > 0) ? 1 : 0));
-            _bakedParticles.StartingPositions[i] = (new TrunkPosition(new Vector3b(x, y, z), new Vector3b((byte)scale[i * 3 + 0], (byte)scale[i * 3 + 1], (byte)scale[i * 3 + 2])));
-        }
-        for (int i = 0; i< DeltaMovement.Count; i++)
-        {
-            var howManyBits = scale[i % (particleSystem.main.maxParticles * 3)];
-            bitstream.WriteByte(DeltaMovement[i], howManyBits);
-            if(i%(particleSystem.main.maxParticles * 3) == 0)
-            {
-                Debug.Log("BitOffset:" + bitstream.BitOffsetPosition / 8);
-            }
-        }
-        Debug.Log(bitstream.BitOffsetPosition);
-        _bakedParticles.MaxBitstream = (int)bitstream.BitOffsetPosition ;
-        SaveObject(_bakedParticles);
-    }
-	// Update is called once per frame
-	void Update () {
-
         if (_deviceController.InTrackRecordMode)
         {
             computeShader.SetFloat("dt", 1.0f / 25f);
@@ -218,6 +220,7 @@ public class ParticlesManager : MonoBehaviour
         {
             computeShader.SetFloat("dt", Time.deltaTime);
         }
+        // Set shader parameters
         computeShader.SetFloat("OrbitX", _deviceController.GetValue(_orbitX));
         computeShader.SetFloat("OrbitY", _deviceController.GetValue(_orbitY));
         computeShader.SetFloat("OrbitZ", _deviceController.GetValue(_orbitZ));
@@ -237,27 +240,29 @@ public class ParticlesManager : MonoBehaviour
         computeShader.SetFloat("WindZ", _deviceController.GetValue(_windZ));
         computeShader.Dispatch(kernelIndex, warpCount, 1, 1);
         particles.GetData(initBuffer);
-            List<Vector3> deltaArray = new List<Vector3>();
-            var particlesArray = new ParticleSystem.Particle[particleSystem.main.maxParticles];
-            var currentAmount = particleSystem.GetParticles(particlesArray);
-            // Change only the particles that are alive
-            for (int i = 0; i < currentAmount; i++)
-            {
-                deltaArray.Add(initBuffer[i].position - particlesArray[i].position);
-                particlesArray[i].position = initBuffer[i].position;
-                particlesArray[i].size = initBuffer[i].position.magnitude * _deviceController.GetValue(_fakeBokeh) * 0.1f + 0.2f;
-                bakeTime = _deviceController.GetAudioSource().time;
-            }
-
-            // Apply the particle changes to the Particle System
-            particleSystem.SetParticles(particlesArray, currentAmount);
+        List<Vector3> deltaArray = new List<Vector3>();
+        var particlesArray = new ParticleSystem.Particle[particleSystem.main.maxParticles];
+        var currentAmount = particleSystem.GetParticles(particlesArray);
+        // Change only the particles that are alive
+        for (int i = 0; i < currentAmount; i++)
+        {
+            deltaArray.Add(initBuffer[i].position - particlesArray[i].position);
+            Vector3 pos = initBuffer[i].position;
+            pos.x = Mathf.Floor(pos.x * 16.0f) / 16.0f;
+            pos.y = Mathf.Floor(pos.y * 16.0f) / 16.0f;
+            pos.z = Mathf.Floor(pos.z * 16.0f) / 16.0f;
+            particlesArray[i].position = pos;
+            particlesArray[i].size = initBuffer[i].position.magnitude * _deviceController.GetValue(_fakeBokeh) * 0.1f + 0.2f;
+            bakeTime = _deviceController.GetAudioSource().time;
+        }
+        // Apply the particle changes to the Particle System
+        particleSystem.SetParticles(particlesArray, currentAmount);
         if (_deviceController.InTrackRecordMode)
         {
             int key = Mathf.FloorToInt(bakeTime * 50.0f);
             if (!_deltaChange.ContainsKey(key) && key > 2)
                 _deltaChange.Add(key, deltaArray);
         }
-
     }
 
     void OnDestroy()
